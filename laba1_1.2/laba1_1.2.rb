84b77a31b4771af63103f6fdefe9c23b75ff6e43
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-02-17 22:39:36 +0300

    лаба 1 задание 1.2

---
+++ laba1_1.2/laba1_1.2.rb
@@ -0,0 +1,20 @@
+puts "What's your name?"
+name = gets.chomp
+puts "Hello, #{name}! What's your favorite programming language?"
+language = gets.chomp
+case language
+when "C++"
+  puts "Хороший выбор. Но скоро будет Ruby"
+when "C#"
+  puts "Скорее всего вы любите ООП, тогда Ruby скоро станет вашем любимым"
+when "F#", "Prolog"
+  puts "4 семестр был вашим любимым. Но скоро вашим любимым семестром будет 6, а любимым языком - Ruby"
+when "Python"
+  puts "Слишком популярный и очевидный ответ. Скоро будет Ruby"
+when "JavaScript"
+  puts "Любите веб, полюбите и Ruby, который скоро станет самым любимым"
+when "Ruby"
+  puts "Вы подлиза!"
+else
+  puts "Скоро будет Ruby"
+end
