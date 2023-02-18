cb88b02a6d94a33a53ed28492b16bd3244fe196f
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-02-18 10:20:43 +0300

    лаба 1 задание 2.3
    исправил nod

--- laba1_2/laba1_2.rb
+++ laba1_2/laba1_2.rb
@@ -10,9 +10,9 @@ def nod(a,b)
       a = a - b
     else
       b = b - a
-      return a
     end
   end
+  return a
   end
 
 def task1(num)
@@ -31,5 +31,27 @@ def task2(num)
   end
 end
 
+def min_del(num)
+  min=num
+  Math.sqrt(num).to_i.downto(2).each {|i| min = i if num%i==0 and i<min}
+  min
+end
+
+def task3(num)
+  max=1
+  num.to_i.downto(2).each {|i| max=i if nod(num, i)!=1 and i%min_del(num)!=0 and i>max}
+  sum=0
+  num.digits.each{|i| sum+=i if i<5}
+  if sum==0 and num.digits.min != 0
+    return "Цифры меньше 5 нет"
+  elsif max==1
+    return "Такого числа нет"
+  else
+    return max*sum
+  end
+end
+
 puts task1(12)
 puts task2(573)
+puts nod(573,571)
+puts task3(573)
