911360f08cab6912b8802677236c068b0a1ea67d
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-02-18 10:44:15 +0300

    лаба 1 задание 3.1 первый положительный

--- laba1_3/laba1_3.rb
+++ laba1_3/laba1_3.rb
@@ -1,6 +1,6 @@
 def minimum(arr)
   min = arr[0]
-  for i in 1..arr.length-1.to_i
+  for i in 0..arr.length-1.to_i
     if arr[i]<min
       min=arr[i]
     end
@@ -8,6 +8,11 @@ def minimum(arr)
   return min
 end
 
-
-
-puts minimum([-2,2,-3,4,5,-1,0])
+def first_pos(arr)
+  for i in 0..arr.length-1.to_i
+    return arr[i] if arr[i]>0
+  end
+  return "Нет такого числа"
+end
+puts minimum([-2,2,-3,4,5,-1,-5])
+puts first_pos([-1,2,-5,-9,-3])
