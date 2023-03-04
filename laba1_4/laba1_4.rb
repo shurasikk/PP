fd2af3ffdb3f4e6a4943560511e841f5a7c1be23
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-03-04 22:55:23 +0300

    лаба 1.4 задание 2

--- laba1_4/laba1_4.rb
+++ laba1_4/laba1_4.rb
@@ -4,3 +4,15 @@ def index_desc(arr)
 end
 
 index_desc([1,5,-3,8,9,-4])
+
+def between_max(arr)
+  max1=arr.index(arr.max)
+  max2 = arr.select {|x| x<arr.max}.max
+  if max1==arr.index(max2)+1 or arr.index(max2)==max1+1
+    puts "Два максимальных элемента массива - соседи"
+  end
+  puts max2
+end
+
+arr=[1,8,-3,9,9,-4]
+between_max(arr)
