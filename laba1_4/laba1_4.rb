05bb406cf4378bf58afe8495bc4f47742e6f548e
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-03-11 01:36:33 +0300

    лаба 1.4 задание 28

--- laba1_4/laba1_4.rb
+++ laba1_4/laba1_4.rb
@@ -10,9 +10,24 @@ def between_max(arr)
   max2 = arr.select {|x| x<arr.max}.max
   if max1==arr.index(max2)+1 or arr.index(max2)==max1+1
     puts "Два максимальных элемента массива - соседи"
+  else arr.each_index {|i| if (i<max1 and i>arr.index(max2)) or (i>max1 and i<arr.index(max2))
+                               puts arr[i] end}
   end
-  puts max2
 end
 
-arr=[1,8,-3,9,9,-4]
+arr=[1,9,-3,7,8,-4]
 between_max(arr)
+
+
+def between_two_maxes(arr)
+  max1=arr.index(arr.max)
+  max2=arr.rindex(arr.max)
+  if max1==max2+1
+    puts "Два максимальных элемента массива - соседи"
+  else arr.each_index{ |i| if i>max1 and i<max2
+                               puts arr[i] end}
+  end
+end
+
+
+between_two_maxes([1,9,-3,7,8,9])
