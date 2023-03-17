2924c2f2229a62e499769aebabfe6c8ec0698178
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-03-17 18:35:40 +0300

    лаба 2.1 задание 3-4

--- laba2/laba2.1/student.rb
+++ laba2/laba2.1/student.rb
@@ -1,55 +1,5 @@
 class Student
-  def initialize(id, surname, name, father_name, phone, tg, mail, git)
-    @ID, @Surname, @Name, @Father_name, @Phone, @Tg, @Mail, @Git = id, surname, name, father_name, phone, tg, mail, git
-  end
-  def ID #геттер
-    @ID
-  end
-  def ID=(id) #сеттер
-    @ID = id
-  end
-  def Surname
-    @Surname
-  end
-  def Surname=(surname)
-    @Surname=surname
-  end
-  def Name
-    @Name
-  end
-  def Name=(name)
-    @Name=name
-  end
-  def Father_name
-    @Father_name
-  end
-  def Father_name=(father_name)
-    @Father_name=father_namename
-  end
-  def Phone
-    @Phone
-  end
-  def Phone=(phone)
-    @Phone=phone
-  end
-  def Tg
-    @Tg
-  end
-  def Tg=(tg)
-    @Tg=tg
-  end
-  def mail
-    @Mail
-  end
-  def Mail=(mail)
-    @Mail=mail
-  end
-  def Git
-    @Git
-  end
-  def Git=(git)
-    @Git=git
-  end
+  attr_accessor :ID, :Name, :Surname, :Father_name, :Git, :Phone, :Tg, :Mail, :Git
   def to_s
     inf=@Name.to_s+" "+@Surname.to_s+" "+@Father_name.to_s
     inf
