e7efa2993820aea3ef424b1f48b565b1297b1c88
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-03-18 09:20:59 +0300

    лаба 2.1 задание 8

--- laba2/laba2.1/student.rb
+++ laba2/laba2.1/student.rb
@@ -12,12 +12,65 @@ class Student
     self.Tg = tg
   end
 
-  def phone_valid?(phone)
-    if phone.match(/^\+?[7,8]{1}\-\d{3}\-\d{3}\-\d{2}\-\d{2}$/)
-      "Match"
-    else
-      "No"
-    end
+  def self.phone_valid?(phone)
+    phone.match(/^\+?[7,8]{1}\-\d{3}\-\d{3}\-\d{2}\-\d{2}$/)
+  end
+
+  def Phone=(phone)
+    raise ArgumentError("Invalid value, phone number's correct form is \"+7(8)-XXX-XXX-XX-XX\"") if !phone.nil? && !Student.phone_valid?(phone)
+    @Phone = phone
+  end
+
+  def self.id_valid?(id)
+    String(id).match(/^\d+$/)
+  end
+
+  def ID=(id)
+    raise ArgumentError("Invalid value, id is digit sequence") if !id.nil? && !Student.id_valid?(id)
+    @ID = id
+  end
+
+  def self.name_valid?(name)
+    name.match(/^[А-Яа-я]+$/)
+  end
+
+  def Name=(name)
+    raise ArgumentError("Invalid value, Name is russian alphabet sequence") if !name.nil? && !Student.name_valid?(name)
+    @Name = name
+  end
+
+  def Surname=(surname)
+    raise ArgumentError("Invalid value, Surname is russian alphabet sequence") if !surname.nil? && !Student.name_valid?(surname)
+    @Surname = surname
+  end
+
+  def Father_name=(father_name)
+    raise ArgumentError("Invalid value, Father name is russian alphabet sequence") if !father_name.nil? && !Student.name_valid?(father_name)
+    @Father_name = father_name
+  end
+
+  def self.acc_valid?(account)
+    account.match(/^@[\w\d-\-_]+$/)
+  end
+
+  def Tg=(tg)
+    raise ArgumentError("Invalid value, Telegram account's correct form is @X \nwhere X is english alphabet sequence") if !tg.nil? && !Student.acc_valid?(tg)
+    @Tg = tg
+  end
+
+  def Git=(git)
+    raise ArgumentError("Invalid value, Git's correct form is @X \nwhere X is english alphabet sequence") if !git.nil? && !Student.acc_valid?(git)
+    @Git = git
+  end
+
+  def self.mail_valid?(mail)
+    mail.match(/^[\w\d]+@[\w]+\.[\w]+$/)
+  end
+
+
+  def Mail=(mail)
+    raise ArgumentError("Invalid value, Mail's correct form is X@X.X \nwhere X is english alphabet sequence") if !mail.nil? && !Student.mail_valid?(mail)
+    @Mail = mail
   end
 
   def to_s
