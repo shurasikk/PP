83957e083c08216b0fc259f313b796352ff07809
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-04-01 02:59:51 +0300

    лаба 2.3 задание 2

--- laba2/laba2.1/data_table.rb
+++ laba2/laba2.1/data_table.rb
@@ -1,10 +1,27 @@
 class Data_table
+
+  attr_reader :rows_count, :cols_count
+
   def initialize(table)
+    self.rows_count = table.length
+    max_cols = 0
+    table.each do |row|
+      max_cols = row.length if row.size > max_cols
+    end
+    self.cols_count = max_cols
     self.table = table
+  end
+
+  def get(row, col)
+    return nil if row >= rows_count || row.negative?
+    return nil if col >= cols_count || col.negative?
+
+    table[row][col]
   end
 
   private
 
   attr_accessor :table
+  attr_writer :rows_count, :cols_count
 
 end
