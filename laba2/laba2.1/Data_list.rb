31e6434c3e5b9135b68913078fcd6464a2536683
shurasikk <90204027+shurasikk@users.noreply.github.com>
2023-04-06 02:02:35 +0300

    лаба 2.3 пункты 3-4

---
+++ laba2/laba2.1/Data_list.rb
@@ -0,0 +1,28 @@
+class Data_List
+
+  private
+  attr_writer :List
+  attr_accessor :Selected_id
+
+  def initialize(objects)
+    @List = objects
+    @Selected_id = []
+  end
+
+  def select(number)
+    @Selected_id.append(@List[number])
+    return nil
+  end
+
+  def get_selected
+    return @Selected_id unless @Selected_id.empty?
+    []
+  end
+
+  def get_names #в наследниках
+  end
+
+  def get_data #в наследниках
+  end
+
+end
